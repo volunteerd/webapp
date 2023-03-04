8fb3ac5ed980f99e930ee920283f5500b490f2bc
Henry Halse <henryhalse@gmail.com>
2023-03-04 13:37:57 -0700

    conditional rendering for login and beginning context

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import Context from './components/UserContext';
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<BrowserRouter><App/></BrowserRouter>);
+root.render(
+<BrowserRouter>
+  <Context>
+    <App/>
+  </Context>
+</BrowserRouter>
+);
